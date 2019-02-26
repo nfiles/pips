27ccf44de62ec2bf0ccda077d64916d1ee4a2d4b
Nathan Files <nathanwfiles@gmail.com>
2019-02-26 09:16:53 -0500

    cleanup and add some doc comments

--- src/expression.rs
+++ src/expression.rs
@@ -1,8 +1,12 @@
+//! Expression Module
+
 use rand::Rng;
 use std::collections::HashMap;
 
 use crate::operators::{advantage, compare, disadvantage, multiply, sum, BinaryOperator};
+use crate::traits::Rollable;
 
+/// Represents a dice roll expression
 #[derive(Clone)]
 pub enum Expression<'a> {
     Die(i32),
@@ -18,46 +22,42 @@ pub enum Expression<'a> {
 use Expression::*;
 
 impl<'a> Expression<'a> {
-    pub fn roll(&self) -> i32 {
+    /// retrieve the operation encapsulated by the given `Expression`,
+    /// represented by a binary operator and left/right expressions
+    fn get_operation(&self) -> Option<(BinaryOperator, &Expression, &Expression)> {
         match self {
-            Constant(num) => *num,
-            Die(num) => rand::thread_rng().gen_range(1, num + 1),
+            Constant(_) => None,
+            Die(_) => None,
 
-            Sum(left, right) => left.roll() + right.roll(),
-            Multiply(left, right) => left.roll() * right.roll(),
-            Advantage(expr) => {
-                let left = expr.roll();
-                let right = expr.roll();
-                if left > right {
-                    left
-                } else {
-                    right
-                }
-            }
-            Disadvantage(expr) => {
-                let left = expr.roll();
-                let right = expr.roll();
-                if left < right {
-                    left
-                } else {
-                    right
-                }
-            }
-            Compare(left, right) => {
-                let left = left.roll();
-                let right = right.roll();
-                if left > right {
-                    1
-                } else if left == right {
-                    0
-                } else {
-                    -1
-                }
-            }
+            Sum(left, right) => Some((sum, left, right)),
+            Multiply(left, right) => Some((multiply, left, right)),
+            Advantage(expr) => Some((advantage, expr, expr)),
+            Disadvantage(expr) => Some((disadvantage, expr, expr)),
+            Compare(left, right) => Some((compare, left, right)),
         }
     }
+}
 
-    pub fn plot(&self) -> HashMap<i32, i32> {
+impl<'a> Rollable for Expression<'a> {
+    /// Get a single value from the roll expression
+    fn roll(&self) -> i32 {
+        // get the root cases out of the way
+        if let Constant(num) = self {
+            return *num;
+        }
+        if let Die(max) = self {
+            return rand::thread_rng().gen_range(1, max + 1);
+        }
+
+        let (operator, left, right) = self
+            .get_operation()
+            .expect("expression does not represent an operation");
+
+        (operator)(&left.roll(), &right.roll())
+    }
+
+    /// Create a list of all possible outcomes and their possibility
+    fn plot(&self) -> HashMap<i32, i32> {
         // get the root cases out of the way
         if let Constant(num) = self {
             return [(*num, 1)].iter().cloned().collect();
@@ -67,16 +67,9 @@ impl<'a> Expression<'a> {
         }
 
         // handle the more complicated expressions
-        let (operator, left, right): (BinaryOperator, _, _) = match self {
-            Sum(left, right) => (sum, left, right),
-            Multiply(left, right) => (multiply, left, right),
-            Advantage(expr) => (advantage, expr, expr),
-            Disadvantage(expr) => (disadvantage, expr, expr),
-            Compare(left, right) => (compare, left, right),
-
-            Constant(_) => panic!("this should never happen"),
-            Die(_) => panic!("this should never happen"),
-        };
+        let (operator, left, right) = self
+            .get_operation()
+            .expect("expression does not represent an operation");
 
         let left = left.plot();
         let right = right.plot();
@@ -105,7 +98,7 @@ mod tests {
 
     #[test]
     fn multiply_produces_correct_plot() {
-        let operation = Expression::Multiply(&Expression::Die(4), &Expression::Die(4));
+        let expression = Expression::Multiply(&Expression::Die(4), &Expression::Die(4));
         let expected: HashMap<i32, i32> = [
             (1, 1),
             (2, 2),
@@ -121,14 +114,14 @@ mod tests {
         .cloned()
         .collect();
 
-        let actual = operation.plot();
+        let actual = expression.plot();
 
         assert_eq!(expected, actual);
     }
 
     #[test]
-    fn two_d6_sum_produces_correct_plot() {
-        let operation = Expression::Sum(&Expression::Die(6), &Expression::Die(6));
+    fn sum_produces_correct_plot() {
+        let expression = Expression::Sum(&Expression::Die(6), &Expression::Die(6));
         let expected: HashMap<i32, i32> = [
             (2, 1),
             (3, 2),
@@ -146,14 +139,14 @@ mod tests {
         .cloned()
         .collect();
 
-        let actual = operation.plot();
+        let actual = expression.plot();
 
         assert_eq!(expected, actual);
     }
 
     #[test]
     fn advantage_produces_correct_plot() {
-        let operation = Expression::Advantage(&Expression::Die(4));
+        let expression = Expression::Advantage(&Expression::Die(4));
         // 1 1 -> 1
         // 1 2 -> 2
         // 1 3 -> 3
@@ -173,14 +166,14 @@ mod tests {
         let expected: HashMap<i32, i32> =
             [(1, 1), (2, 3), (3, 5), (4, 7)].iter().cloned().collect();
 
-        let actual = operation.plot();
+        let actual = expression.plot();
 
         assert_eq!(expected, actual);
     }
 
     #[test]
     fn disadvantage_produces_correct_plot() {
-        let operation = Expression::Disadvantage(&Expression::Die(4));
+        let expression = Expression::Disadvantage(&Expression::Die(4));
         // 1 1 -> 1
         // 1 2 -> 1
         // 1 3 -> 1
@@ -200,14 +193,14 @@ mod tests {
         let expected: HashMap<i32, i32> =
             [(1, 7), (2, 5), (3, 3), (4, 1)].iter().cloned().collect();
 
-        let actual = operation.plot();
+        let actual = expression.plot();
 
         assert_eq!(expected, actual);
     }
 
     #[test]
     fn contest_produces_correct_plot() {
-        let operation = Expression::Compare(&Expression::Die(2), &Expression::Die(3));
+        let expression = Expression::Compare(&Expression::Die(2), &Expression::Die(3));
 
         // 1 1 -> 0
         // 1 2 -> -1
@@ -217,7 +210,21 @@ mod tests {
         // 2 3 -> -1
         let expected: HashMap<i32, i32> = [(-1, 3), (0, 2), (1, 1)].iter().cloned().collect();
 
-        let actual = operation.plot();
+        let actual = expression.plot();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn compare_produces_correct_plot() {
+        let expression = Expression::Compare(&Expression::Die(3), &Expression::Constant(2));
+
+        // 1 2 -> -1
+        // 2 2 -> 0
+        // 3 2 -> 1
+        let expected: HashMap<i32, i32> = [(-1, 1), (0, 1), (1, 1)].iter().cloned().collect();
+
+        let actual = expression.plot();
 
         assert_eq!(expected, actual);
     }
