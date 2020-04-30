93072f15b2563699d5c05ede2498f3e6fdf9ccbf
Nathan Files <nathanwfiles@gmail.com>
2020-04-30 11:32:56 -0400

    adds prettier

--- pips-web/src/app/app.component.ts
+++ pips-web/src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import('pips-wasm')
-  .then(pips => {
-    (window as any)['roll'] = (input: string) => pips.roll(input);
-    (window as any)['plot'] = (input: string) => pips.plot(input);
-  })
-  .catch(err => {
-    console.error('error importing pips-wasm', err);
-  });
+    .then((pips) => {
+        (window as any)['roll'] = (input: string) => pips.roll(input);
+        (window as any)['plot'] = (input: string) => pips.plot(input);
+    })
+    .catch((err) => {
+        console.error('error importing pips-wasm', err);
+    });
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pips-ng';
+    title = 'pips-ng';
 }
