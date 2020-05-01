38d24541ee002ca848c4cf9d1d63a28c4309dce3
Nathan Files <nathanwfiles@gmail.com>
2020-05-01 09:58:00 -0400

    removes global pips-wasm debug functions

--- pips-web/src/app/pips.service.ts
+++ pips-web/src/app/pips.service.ts
@@ -1,15 +1,6 @@
 import { Injectable } from '@angular/core';
 
-const pips$ = import('pips-wasm')
-    .then((pips) => {
-        (window as any)['roll'] = (input: string) => pips.roll(input);
-        (window as any)['plot'] = (input: string) => pips.plot(input);
-        return pips;
-    })
-    .catch((err) => {
-        console.error('error importing pips-wasm', err);
-        throw err;
-    });
+const pips$ = import('pips-wasm');
 
 @Injectable({
     providedIn: 'root',
