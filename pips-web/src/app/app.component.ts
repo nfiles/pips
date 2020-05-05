826a5568860e36bfcd341c571b6c4a4b5b34079b
Nathan Files <nathanwfiles@gmail.com>
2020-05-05 17:41:58 -0400

    adds package version to navbar

--- pips-web/src/app/app.component.ts
+++ pips-web/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { version } from '../../package.json';
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+    readonly version = version;
     isMenuCollapsed = true;
 }
