e028ae223250cf010a748f07d4fd5e5ee93aa08f
Nathan Files <nathanwfiles@gmail.com>
2021-03-12 15:18:14 -0500

    fixes duplicate .wasm extension bug

--- build-wasm.sh
+++ build-wasm.sh
@@ -9,5 +9,5 @@ wasm-pack build pips-wasm --out-dir pkg
 
 # add the .wasm extension to the first line to make
 # the angular compiler happy
-sed -e '1 s/pips_wasm_bg/pips_wasm_bg.wasm/' \
+sed -e '1 s/pips_wasm_bg"/pips_wasm_bg.wasm"/' \
     -i pips-wasm/pkg/pips_wasm.js
