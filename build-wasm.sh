c1fc5e10393a37bca08d961ac9b3cf20b010abe9
Nathan Files <nathanwfiles@gmail.com>
2021-03-16 15:30:55 -0400

    includes all necessary files in `pips-wasm` npm package
    - wasm-pack has a bug that caused it not to include all generated files
      in the "files" array of package.json. This update hacks in a manual
      remedy for that in build-wasm.sh

--- build-wasm.sh
+++ build-wasm.sh
@@ -11,3 +11,19 @@ wasm-pack build pips-wasm --out-dir pkg
 # the angular compiler happy
 sed -e '1 s/pips_wasm_bg"/pips_wasm_bg.wasm"/' \
     -i pips-wasm/pkg/pips_wasm.js
+
+# make sure all files are included in package.json
+pushd pips-wasm/pkg > /dev/null
+
+PACKAGE_JSON="$(cat package.json)"
+FILES=$(ls *.ts *.js *.wasm)
+for file in $FILES; do
+	# make sure all input files are included in package.json ".files"
+	if [ "$(jq ".files | all(. != \"$file\")" <<<"$PACKAGE_JSON")" == "true" ]; then
+		PACKAGE_JSON="$(jq ".files |= . + [\"$file\"]" <<<"$PACKAGE_JSON")"
+	fi
+done
+
+echo "$PACKAGE_JSON" > package.json
+
+popd > /dev/null
